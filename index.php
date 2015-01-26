dfe5379cd035f64ce8a3321d4b33fa1b96727806
Justin Hersey <justin.hersey@gmail.com>
2015-01-25 18:17:05 -0800

    add styles

--- index.php
+++ index.php
@@ -9,38 +9,30 @@
 	<div class="row">
 		<div class="col-12 centered-text">
 			<h2>The work</h2>
-			<p>At the start of each project, my team and I define </p>
+			<p class="lead-in">Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-4 centered-text">
 			<img class="project-image" src="build/images/airstream1.jpg" alt="Airstream" />
 		</div>
-	</div>
-	<div class="row">
 		<div class="col-4 centered-text">
 			<img class="project-image" src="build/images/airstream2.jpg" alt="Airstream" />
 		</div>
-	</div>
-	<div class="row">
 		<div class="col-4 centered-text">
 			<img class="project-image" src="build/images/airstream3.jpg" alt="Airstream" />
 		</div>
 	</div>
 	<div class="row">
-		<div class="col-3">
+		<div class="col-4">
 			<h3 class="title">Perspiciatis</h3>
 			<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
 		</div>
-		<div class="col-3">
+		<div class="col-4">
 			<h3 class="title">Quisquam</h3>
 			<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
 		</div>
-		<div class="col-3">
-			<h3 class="title">Nostrum</h3>
-			<p>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
-		</div>
-		<div class="col-3">
+		<div class="col-4">
 			<h3 class="title">Accusamus</h3>
 			<p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.</p>
 		</div>
