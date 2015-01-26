c4163557e2f02752f657ff4ed39fced2f5a97ab6
Justin Hersey <justin.hersey@gmail.com>
2015-01-25 21:11:36 -0800

    add styles

--- includes/footer.php
+++ includes/footer.php
@@ -9,7 +9,7 @@
 	</footer>
 
 	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-	<script type="text/javascript" src="/build/js/scripts.min.js"></script>
+	<script type="text/javascript" src="build/js/scripts.min.js"></script>
 	<script type="text/javascript">
 		$(document).ready(function() {
 			navToggle();
