392ab4a51d1ce264ef3ca1883ff65d2c4e8cbf16
Justin Hersey <justin.hersey@gmail.com>
2015-01-25 17:22:34 -0800

    add styles

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 	<div class="row">
 		<div class="col-12 centered-text">
 			<h2>The work</h2>
-			<p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.</p>
+			<p>At the start of each project, my team and I define </p>
 		</div>
 	</div>
 	<img class="project-image" src="build/images/airstream1.jpg" alt="Airstream" />
