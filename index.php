b7f94272cfa126149e8df416b8cbad5c6eadc29b
Justin Hersey <justin.hersey@gmail.com>
2015-01-25 18:24:05 -0800

    add styles

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 	</div>-->	
 	<div class="row">
 		<div class="col-12 centered-text">
-			<p class="lead-in">Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+			<p class="lead-in"><span class="brand-emphasis">Airstream</span> has a unique history. A history not many other companies can compare to. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
 		</div>
 	</div>
 	<div class="row">
