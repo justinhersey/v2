145b4e10b7845bbcd713c7367dccad8d7fd0b54d
Justin Hersey <justin.hersey@gmail.com>
2015-01-28 23:05:54 -0800

    commit

--- includes/footer.php
+++ includes/footer.php
@@ -2,8 +2,8 @@
 		<div class="row">
 			<div class="col-12">
 				<ul class="unstyled inline social-icons">
-					<li><img src="build/images/linkedin.svg" alt="LinkedIn" /></li>
-					<li><img src="build/images/twitter.svg" alt="Twitter" /></li>
+					<li><a href="https://www.linkedin.com/pub/justin-hersey/9/918/4" title="LinkedIn"><img src="build/images/linkedin.svg" alt="LinkedIn" /></a></li>
+					<li><a href="https://twitter.com/justinhersey" title="Twitter"><img src="build/images/twitter.svg" alt="Twitter" /></a></li>
 				</ul>
 			</div>
 		</div>
