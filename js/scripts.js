5169a67a7e8cfa2f2d8ac0034cef2ed9b628b8b9
Justin Hersey <justin.hersey@gmail.com>
2015-01-31 12:53:11 -0800

    commit

--- js/scripts.js
+++ js/scripts.js
@@ -33,7 +33,7 @@ function navToggle() {
 					var $this = $(this);
 					setTimeout(function() {
 						$this.addClass(animateClasses);
-					}, 50*i);
+					}, 100*i);
 				});
 			} else {
 				navListItem.removeClass(animateClasses);
