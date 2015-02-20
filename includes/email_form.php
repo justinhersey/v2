e60aaf5915c441b5cfed7bd4b19b6fc54dd4652e
Justin Hersey <justin.hersey@gmail.com>
2015-02-19 21:21:48 -0800

    commit

--- includes/email_form.php
+++ includes/email_form.php
@@ -1,14 +1,13 @@
-<form id="form1" class="email-form" name="form1" accept-charset="UTF-8" autocomplete="off" enctype="multipart/form-data" method="post" novalidate action="https://justinhersey.wufoo.com/forms/zfr4bdj0h75w7t/#public">
-	<div class="col-8">
+<div class="col-12">
+	<h2>Ask me a question</h2>
+	<form id="form1" class="email-form" name="form1" accept-charset="UTF-8" autocomplete="off" enctype="multipart/form-data" method="post" novalidate action="https://justinhersey.wufoo.com/forms/zfr4bdj0h75w7t/#public">
 		<textarea id="Field1" name="Field1" spellcheck="true"rows="5" cols="50" tabindex="1" onkeyup="" required placeholder="What's your question?"></textarea>
 		<input id="Field2" name="Field2" type="email" spellcheck="false" value="" maxlength="255" tabindex="2" placeholder="Your email address" />
-	</div>
-	<div class="col-4">
 		<input id="saveForm" name="saveForm" class="button" type="submit" value="Send question" />
-	</div>
-	<div class="hide">
-		<label for="comment">Do Not Fill This Out</label>
-		<textarea name="comment" id="comment" rows="1" cols="1"></textarea>
-		<input type="hidden" id="idstamp" name="idstamp" value="eIRPhdWeM083Uxa3AIZN3JB8zGVtoWwRhsvv4uwCovA=" />
-	</div>
-</form>
+		<div class="hide">
+			<label for="comment">Do Not Fill This Out</label>
+			<textarea name="comment" id="comment" rows="1" cols="1"></textarea>
+			<input type="hidden" id="idstamp" name="idstamp" value="eIRPhdWeM083Uxa3AIZN3JB8zGVtoWwRhsvv4uwCovA=" />
+		</div>
+	</form>
+</div>
--- index.php
+++ index.php
@@ -10,9 +10,6 @@
 			<h3 class="title">Concept</h3>
 			<p>We begin with the creative brief. For many projects, this is a succinct document outlining the scope and deliverables of the project. The Hobie Livewell XL required nothing more than a one-page illustrated wireframe of sorts to develop the layout and photography art direction.</p>
 			<p>I am involved in the process from concept to completion acting in various roles from project manager, art director and lead front-end designer. Each project typically involves working with a small team to execute the various stages of development. This includes working with our industrial designers, engineers and product managers to develop the project scope.</p>
-			<img src="build/images/livewell-xl.jpg" alt="Livewell XL" />
-			<img src="build/images/livewell-xl-final.jpg" alt="Livewell XL" />
-			<img src="build/images/twist-stow.gif" alt="Livewell XL" />
 		</div>
 		<div class="col-4">
 			<h3 class="title">Project Lead</h3>
@@ -22,10 +19,13 @@
 			<h3 class="title">Accusamus</h3>
 			<p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.</p>
 		</div>
-	</div>	
+	</div>
 	<div class="row">
-		<div class="col-12 centered-text">
-			<h2>Ask me a question</h2>
+		<div class="col-6">
+			<img src="build/images/livewell-xl.jpg" alt="Livewell XL Sketch" />
+		</div>
+		<div class="col-6">
+			<img src="build/images/livewell-xl-final.jpg" alt="Livewell XL Final Product" />
 		</div>
 	</div>
 	<div class="row">
