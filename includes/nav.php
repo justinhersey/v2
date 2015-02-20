d1809f6b64dcf1157e248efc2f3b314caa153394
Justin Hersey <justin.hersey@gmail.com>
2015-02-19 20:38:34 -0800

    commit

--- includes/nav.php
+++ includes/nav.php
@@ -1,14 +1,16 @@
 <nav class="nav">
 	<div class="row">
-		<div class="site-name">Justin Hersey</div>
-		<a id="nav-toggle" class="nav-toggle" href="#"><span></span></a>
+		<div class="col-12">
+			<div class="site-name">Justin Hersey</div>
+			<a id="nav-toggle" class="nav-toggle" href="#"><span></span></a>
+			<ul class="hide nav-list list-unstyled">
+				<li<?php if ($thisPage=="Airstream") echo " class=\"active\""; ?>><a href="/airstream/">Airstream</a></li>
+				<li<?php if ($thisPage=="Hobie Cat Company") echo " class=\"active\""; ?>><a href="/hobie-cat-company/">Hobie Cat<sup>&reg;</sup> Company</a></li>
+				<li<?php if ($thisPage=="Malibu Boats") echo " class=\"active\""; ?>><a href="/malibu-boats/">Malibu Boats</a></li>
+				<li<?php if ($thisPage=="Smith Action Optics") echo " class=\"active\""; ?>><a href="/smith-action-optics/">Smith Action Optics</a></li>
+				<li<?php if ($thisPage=="Marvin Road Mini Storage") echo " class=\"active\""; ?>><a href="/marvin-road-mini-storage/">Marvin Road Mini Storage</a></li>
+				<li<?php if ($thisPage=="Friday Harbor House") echo " class=\"active\""; ?>><a href="/friday-harbor-house/">Friday Harbor House</a></li>
+			</ul>
+		</div>
 	</div>
-	<ul class="hide nav-list list-unstyled">
-		<li<?php if ($thisPage=="Airstream") echo " class=\"active\""; ?>><a href="/airstream/">Airstream</a></li>
-		<li<?php if ($thisPage=="Hobie Cat Company") echo " class=\"active\""; ?>><a href="/hobie-cat-company/">Hobie Cat<sup>&reg;</sup> Company</a></li>
-		<li<?php if ($thisPage=="Malibu Boats") echo " class=\"active\""; ?>><a href="/malibu-boats/">Malibu Boats</a></li>
-		<li<?php if ($thisPage=="Smith Action Optics") echo " class=\"active\""; ?>><a href="/smith-action-optics/">Smith Action Optics</a></li>
-		<li<?php if ($thisPage=="Marvin Road Mini Storage") echo " class=\"active\""; ?>><a href="/marvin-road-mini-storage/">Marvin Road Mini Storage</a></li>
-		<li<?php if ($thisPage=="Friday Harbor House") echo " class=\"active\""; ?>><a href="/friday-harbor-house/">Friday Harbor House</a></li>
-	</ul>
 </nav>
--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 	<?php include("includes/nav.php"); ?>
 	<div class="row">
 		<div class="col-12">
-			<p><span class="brand-emphasis">Hobie Cat<sup>&reg;</sup></span></p>
 			<p class="lead-in"><span class="brand-emphasis">Hobie Cat&rsquo;s</span> website was a ground up redesign. I was tasked with developing much of the way we market our products on our website. I develop the content for the project by first interviewing our product managers, product designers and engineers. In collaboration </p>
 		</div>
 	</div>
