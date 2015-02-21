bb80d9b240a65c178a0426fa3ead20c7264146f7
Justin Hersey <justin.hersey@gmail.com>
2015-02-21 14:39:05 -0800

    commit

--- includes/header.php
+++ includes/header.php
@@ -2,8 +2,8 @@
 <html>
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="../build/css/stylesheet.css" rel="stylesheet" type="text/css">
-	<?php include("fonts.php"); ?>
+    <link href="/build/css/stylesheet.css" rel="stylesheet" type="text/css">
+	<?php include(dirname(__FILE__)."/fonts.php"); ?>
     <script>document.cookie='resolution='+Math.max(screen.width,screen.height)+'; path=/';</script>
     <!--<script>document.cookie='resolution='+480+'; path=/';</script>-->
     <!--[if lt IE 10]>
