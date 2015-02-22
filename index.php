36ece780b60d8f03a9b3ca3181004f91d22e3696
Justin Hersey <justin.hersey@gmail.com>
2015-02-21 22:09:39 -0800

    commit

--- index.php
+++ index.php
@@ -5,9 +5,17 @@
 			<p class="lead-in"><span class="brand-emphasis">Hello, I'm Justin.</span> I'm a designer with an education in graphic design working in web marketing. I identify and define opportunities, and develop solutions by leading teams through collaboration.</p>
 		</div>
 	</div>
-	<div class="row">
-		<div class="col-12 columns">
-			<h2>Hand-picked Work</h2>
+	<div class="row block-grid work">
+		<div class="col-4 columns">
+			<img src="build/images/livewell-xl.jpg" alt="Hobie Cat Company" />
+		</div><!--
+		--><div class="col-4 columns">
+			<img src="build/images/livewell-xl-final.jpg" alt="Livewell XL Final Product" />
+		</div><!--
+		--><div class="col-4 columns">
+			<h2>Hobie Cat Company</h2>
+			<p>Design &amp; Frontend</p>
+			<a class="button" href="">Review</a>
 		</div>
 	</div>
 	<div class="row block-grid work">
@@ -18,23 +26,26 @@
 			<img src="build/images/livewell-xl-final.jpg" alt="Livewell XL Final Product" />
 		</div><!--
 		--><div class="col-4 columns">
-			<div class="title">Hobie Cat Company</div>
+			<h2>Malibu Boats</h2>
+			<p>Design &amp; Photography Art Direction</p>
+			<a class="button" href="">Review</a>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-12 columns">
+			<h2>Malibu Boats</h2>
+		</div>
+	</div>
+	<div class="row block-grid work">
+		<div class="col-4 columns">
+			<img src="build/images/livewell-xl.jpg" alt="Hobie Cat Company" />
+		</div><!--
+		--><div class="col-4 columns">
+			<img src="build/images/livewell-xl-final.jpg" alt="Livewell XL Final Product" />
+		</div><!--
+		--><div class="col-4 columns">
 			<p>We begin with the creative brief. For many projects, this is a succinct document outlining the scope and deliverables of the project. The Hobie Livewell XL required nothing more than a one-page illustrated wireframe of sorts to develop the layout and photography art direction.</p>
 			<p><a class="button" href="">Review</a></p>
-		</div><!--
-		--><div class="col-4 columns">
-			<h3 class="title">Hobie Cat Company</h3>
-			<p>Nearly every design is sketched on paper before any photography art direction is given. We begin with the creative brief. For many projects, this is a succinct document outlining the scope and deliverables of the project. The Hobie Livewell XL required nothing more than a one-page illustrated wireframe of sorts to develop the layout and photography art direction.</p>
-			<p>I am involved in the process from concept to completion acting in various roles from project manager, art director and lead front-end designer. Each project typically involves working with a small team to execute the various stages of development. This includes working with our industrial designers, engineers and product managers to develop the project scope.</p>
-		</div><!--
-		--><div class="col-4 columns">
-			<h3 class="title">Project Lead</h3>
-			<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
-		</div><!--
-		--><div class="col-4 columns">
-			<h3 class="title">Accusamus</h3>
-			<p>We begin with the creative brief. For many projects, this is a succinct document outlining the scope and deliverables of the project. The Hobie Livewell XL required nothing more than a one-page illustrated wireframe of sorts to develop the layout and photography art direction.</p>
-			<p>I am involved in the process from concept to completion acting in various roles from project manager, art director and lead front-end designer. Each project typically involves working with a small team to execute the various stages of development. This includes working with our industrial designers, engineers and product managers to develop the project scope.</p>
 		</div>
 	</div>
 	<div class="row">
