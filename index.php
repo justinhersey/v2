c22bdd7fe496d4ec1e54f0c827dff71ff4ee491f
Justin Hersey <justin.hersey@gmail.com>
2015-03-01 14:32:56 -0800

    commit

--- index.php
+++ index.php
@@ -6,6 +6,10 @@
 		</div>
 	</div>
 	<div class="row block-grid work">
+		<div class="col-12 columns">
+			<h2>Hobie Cat<sup>&reg;</sup> Company</h2>
+			<p>Design &amp; Frontend</p>
+		</div>
 		<div class="col-4 columns">
 			<img src="build/images/livewell-xl.jpg" alt="Hobie Cat Company" />
 		</div><!--
@@ -13,12 +17,14 @@
 			<img src="build/images/livewell-xl-final.jpg" alt="Livewell XL Final Product" />
 		</div><!--
 		--><div class="col-4 columns">
-			<h2>Hobie Cat<sup>&reg;</sup> Company</h2>
-			<p>Design &amp; Frontend</p>
-			<a class="button" href="/hobie-cat-company/">Review Project</a>
+			<img src="build/images/livewell-xl-final.jpg" alt="Livewell XL Final Product" />
 		</div>
 	</div>
 	<div class="row block-grid work">
+		<div class="col-12 columns">
+			<h2>Malibu Boats</h2>
+			<p>Design &amp; Photography Art Direction</p>
+		</div>
 		<div class="col-4 columns">
 			<img src="build/images/malibu-sketches.jpg" alt="Malibu Boats" />
 		</div><!--
@@ -26,33 +32,7 @@
 			<img src="build/images/malibu-top.jpg" alt="Malibu Boats" />
 		</div><!--
 		--><div class="col-4 columns">
-			<h2>Malibu Boats</h2>
-			<p>Design &amp; Photography Art Direction</p>
-			<a class="button" href="/malibu-boats/">Review Project</a>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-12 columns">
-			<h2>Malibu Boats</h2>
-		</div>
-	</div>
-	<div class="row block-grid work">
-		<div class="col-4 columns">
-			<img src="build/images/livewell-xl.jpg" alt="Hobie Cat Company" />
-		</div><!--
-		--><div class="col-4 columns">
 			<img src="build/images/livewell-xl-final.jpg" alt="Livewell XL Final Product" />
-		</div><!--
-		--><div class="col-4 columns">
-			<p>We begin with the creative brief. For many projects, this is a succinct document outlining the scope and deliverables of the project. The Hobie Livewell XL required nothing more than a one-page illustrated wireframe of sorts to develop the layout and photography art direction.</p>
-			<p><a class="button" href="">Review</a></p>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-12 columns">
-			<div class="email-form">
-				<?php include("includes/email_form.php"); ?>
-			</div>
 		</div>
 	</div>
 <?php include("includes/footer.php"); ?>
