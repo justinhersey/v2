24e9ca1beb9dca58cea307c29481b97883c52883
Justin Hersey <justin.hersey@gmail.com>
2015-08-08 15:09:11 -0700

    add logo

--- includes/nav.php
+++ includes/nav.php
@@ -1,7 +1,7 @@
 <nav class="nav">
 	<div class="row">
 		<div class="col-12 columns">
-			<a href="/"><img src="../build/images/jh-logo.png" /><div class="site-name">I am Justin Hersey</div></a>
+			<div class="site-name"><a href="/"><!--<img src="../build/images/jh-logo.svg" />--><span>I am Justin Hersey</span></a></div>
 			<!--<a id="nav-toggle" class="nav-toggle" href="#"><span></span></a>-->
 		</div>
 		<div class="col-12 columns">
